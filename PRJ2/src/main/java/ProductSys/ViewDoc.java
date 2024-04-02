f5e2ddf16b4c17bd14fe05d7cdc030f986254f1c
MigoVseti <137178864+MigoVseti@users.noreply.github.com>
2024-04-02 12:30:28 +0300

    addMethods
    added new methods DeleteDoc ShowAll

--- PRJ2/src/main/java/ProductSys/ViewDoc.java
+++ PRJ2/src/main/java/ProductSys/ViewDoc.java
@@ -2,30 +2,32 @@ package ProductSys;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class ViewDoc {
     private List<Document> documents = new ArrayList<>();
 
-    public ViewDoc() {
+    public void ViewDoc() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter Order name: ");
+        System.out.println("Enter Order name: ");
         String orderName = scanner.nextLine();
-        System.out.print("Enter Order quantity: ");
-        int orderQuantity = scanner.nextInt();
+        System.out.println("Enter Order quantity: ");
+        int orderQuantity = Integer.parseInt(scanner.nextLine());
         Order order = new Order(orderName, orderQuantity);
 
-        System.out.print("Enter Invoice amount: ");
-        int invoiceAmount = scanner.nextInt();
+        System.out.println("Enter Invoice amount: ");
+        int invoiceAmount = Integer.parseInt(scanner.nextLine());
         Invoice invoice = new Invoice(invoiceAmount);
 
-        System.out.print("Enter Payment total cost: ");
-        int paymentTotalCost = scanner.nextInt();
+        System.out.println("Payment total cost: ");
+        int paymentTotalCost = orderQuantity * invoiceAmount;
+        System.out.println(paymentTotalCost);
         Payment payment = new Payment(paymentTotalCost);
 
-        System.out.print("Enter Waybill payment type: ");
-        String waybillPaymentType = scanner.next();
+        System.out.println("Enter Waybill payment type(Нал/БезНал): ");
+        String waybillPaymentType = scanner.nextLine();
         Waybill waybill = new Waybill(waybillPaymentType);
 
         order.setInvoice(invoice);
@@ -33,9 +35,10 @@ public class ViewDoc {
         payment.setWaybill(waybill);
 
         documents.add(order);
+
     }
 
-    public Document getDocumentById(int id) {
+    private Document getDocumentById(int id) {
         for (Document doc : documents) {
             if (doc.getId() == id) {
                 return doc;
@@ -43,6 +46,7 @@ public class ViewDoc {
         }
         return null; // Document not found
     }
+
 
     public void displayDocumentById(int id) {
         Document doc = getDocumentById(id);
@@ -52,5 +56,20 @@ public class ViewDoc {
         } else {
             System.out.println("Document with ID " + id + " not found.");
         }
+    }
+
+    public void deleteDoccumentByID(int id){
+        Document document = getDocumentById(id);
+        if (document != null){
+            documents.remove(document);
+            System.out.println("Document has been deleted");
+        }
+        else{
+            System.out.println("Document with ID " + id + " not found.");
+        }
+    }
+
+    public void showAllDocuments(){
+        System.out.println(documents.toString());
     }
 }
--- PRJ2/src/test/java/App.java
+++ PRJ2/src/test/java/App.java
@@ -7,9 +7,31 @@ public class App {
     public static void main(String[] args) {
         ViewDoc viewDocument = new ViewDoc();
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter id: ");
-        int id = scanner.nextInt();
-        viewDocument.displayDocumentById(id);
-        System.out.println((Document) viewDocument.getDocumentById(id));
+
+        System.out.println("1.Add document\n2.Display document by id\n3.Delete document\n4.Show all documents\n5.Exit");
+
+        while (true){
+            int opp = scanner.nextInt();
+            switch (opp){
+                case 1:
+                    viewDocument.ViewDoc();
+                    break;
+                case 2:
+                    System.out.print("Enter id: ");
+                    int id1 = scanner.nextInt();
+                    viewDocument.displayDocumentById(id1);
+                    break;
+                case 3:
+                    System.out.print("Enter id: ");
+                    int id2 = scanner.nextInt();
+                    viewDocument.deleteDoccumentByID(id2);
+                    break;
+                case 4:
+                    viewDocument.showAllDocuments();
+                case 5:
+                    System.exit(0);
+            }
+        }
+
     }
 }
